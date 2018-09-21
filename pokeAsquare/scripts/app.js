c39f1591ea11932765044eb544bc8ce95a564cbc
Jawad Alikhel <jawadalikhel@Jawads-MacBook-Pro.local>
2018-09-21 11:36:13 -0500

    created cereate a branch

--- pokeAsquare/scripts/app.js
+++ pokeAsquare/scripts/app.js
@@ -1,1 +1,19 @@
+$('button').on('click', () => {
+	console.log('click works');
+	// when the user clicks the button,
+	// popilate the squares
+	creteSquares(30);
+})
 
+// create a function named creteSquares,
+// that takes a parameter of an arbitary 
+// number and creates divs according to the 
+
+const creteSquares = (numberOfSquares) =>{
+	for(let i =0; i < numberOfSquares; i++){
+		//creates divs
+		const $div = $('<div/>');
+		//attach to the square class
+		$('.squares').append($div);
+	}
+}
